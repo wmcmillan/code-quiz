1fc75df26d88ff7835843d129b755c5cb8a4a92e
Wes <wmcmillan1012@gmail.com>
2020-09-16 01:22:24 -0700

    added bootstrap card to contain questions and answer buttons

--- script.js
+++ script.js
@@ -12,4 +12,5 @@ function beginQuiz () {
     gameTimer();
     document.getElementById("rules").style.display = "none";
     document.getElementById("start").style.display = "none";
+    document.getElementById("answerButtons").style.display = "";
 }
