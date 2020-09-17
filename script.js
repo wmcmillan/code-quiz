5861437ba42ea31c1dd5a015060b89ec02809c00
Wesley McMillan <wes@Wesleys-MacBook-Pro.local>
2020-09-16 18:56:36 -0700

    starting writing timer function

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 var startButton = document.querySelector("#start");
 var rules = document.querySelector("#rules")
+var timer = document.querySelector("timer")
 
 
 startButton.addEventListener("click", beginQuiz)
-
-function gameTimer () {
-
-}
 
 function beginQuiz () {
     gameTimer();
@@ -15,4 +12,24 @@ function beginQuiz () {
     document.getElementById("answerButtons").style.display = "";
     document.getElementById("questions").style.display = "";
 
+}
+
+function gameTimer () {
+    var timeLeft = 10;
+
+    var timeInterval = setInterval(function(){
+        timer.textContent = timeLeft + "seconds remain";
+        timeLeft--;
+
+        if (timeLeft == 0) {
+            timer.textContent= "";
+            gameOver();
+            clearInterval(timeInterval);
+            console.log(timeLeft);
+        }
+    }, 1000);
+}
+
+function gameOver() {
+    document.getElementById("gameOver").style.display = "";
 }
